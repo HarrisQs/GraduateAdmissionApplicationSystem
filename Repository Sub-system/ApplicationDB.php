a28dfff1ccf974166fd040db372e0d97a0abde3a
HarrisQs <harris32916@gmail.com>
2016-05-13 06:43:01 +0800

    ApplcationDB initialize

---
+++ Repository Sub-system/ApplicationDB.php
@@ -0,0 +1,57 @@
+<?php
+	include_once "ConnectDB.php"
+	class ApplicationDB
+	{
+		private $DataBase;
+
+		function __construct()
+		{
+
+		}
+		function __destruct()
+		{
+
+		}
+		public function SaveBasicData($basicData, $account)
+		{
+
+		}
+		public function SaveTransrcipts($transcripts, $account)
+		{
+
+		}
+		public function SaveTeacherEmail($TeacherEmail, $account)
+		{
+
+		}
+		public function SaveRecommendationLetter($fileName, $account)
+		{
+
+		}
+		public function SaveSOP($SOP, $account)
+		{
+
+		}
+		public function SaveProgramSelection ($programSelection, $account)
+		{
+
+		}
+		public function SaveCV($CV, $account)
+		{
+
+		}
+		public function SaveState($state, $account)
+		{
+
+		}
+		public function GetLastHistory($account)
+		{
+
+		}
+		public function GetApplcation($school)
+		{
+			
+		}
+	}
+
+?>
