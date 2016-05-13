18ccae21c9d5f4b9d82bcd124f994fb92c91c9cb
HarrisQs <harris32916@gmail.com>
2016-05-13 18:17:31 +0800

    RecommendationLetter basic frame.

---
+++ Upload Sub-system/RecommendationLetter.php
@@ -0,0 +1,27 @@
+<?php
+	include_once"../Respository Sub-system/CennectDB.php"
+	class RecommendationLetter
+	{
+		private $DBLink;
+		private $FileName;
+		private $FileAddress;
+		function __construct()
+		{
+
+		}
+		function __destruct()
+		{
+
+		}
+		public function SaveRecommendationLetter($fileName)
+		{
+
+		}
+		public function SaveToDB()
+		{
+			
+		}
+
+	}
+
+?>
