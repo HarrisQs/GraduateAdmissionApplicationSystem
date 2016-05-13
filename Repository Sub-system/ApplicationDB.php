99f0828719d3628d743c9083cb8e52f52373aaca
HarrisQs <harris32916@gmail.com>
2016-05-13 15:29:54 +0800

    Save all application

--- Repository Sub-system/ApplicationDB.php
+++ Repository Sub-system/ApplicationDB.php
@@ -23,34 +23,63 @@
 								'$json_Result->Department'
 								);";
 			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function SaveTransrcipts($transcripts, $account)
 		{
-
+			$command = "UPDATE application_data
+						SET Transcipts = '$transcripts'
+						WHERE account = '$account';";
+			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function SaveTeacherEmail($TeacherEmail, $account)
 		{
-
+			$command = "UPDATE application_data
+						SET TeacherEmail = '$TeacherEmail'
+						WHERE account = '$account';";
+			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function SaveRecommendationLetter($fileName, $account)
 		{
-
+			$command = "UPDATE application_data
+						SET RecommendationLetter = '$fileName'
+						WHERE account = '$account';";
+			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function SaveSOP($SOP, $account)
 		{
-
+			$command = "UPDATE application_data
+						SET SOP = '$SOP'
+						WHERE account = '$account';";
+			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function SaveProgramSelection ($programSelection, $account)
 		{
-
+			$command = "UPDATE application_data
+						SET ProgramSelection = '$programSelection'
+						WHERE account = '$account';";
+			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function SaveCV($CV, $account)
 		{
-
+			$command = "UPDATE application_data
+						SET CV = '$CV'
+						WHERE account = '$account';";
+			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function SaveState($state, $account)
 		{
-
+			$command = "UPDATE application_data
+						SET Status = '$state'
+						WHERE account = '$account';";
+			$this->DataBase->DB_Insert($command);
+			return true;
 		}
 		public function GetLastHistory($account)
 		{
--- Repository Sub-system/Test.php
+++ Repository Sub-system/Test.php
@@ -4,8 +4,7 @@ include_once "ApplicationDB.php";
 $carPart = new ApplicationDB();
 
 //$carPart -> AddNewAccount("11112");
-$carPart -> SaveBasicData('{"account": "12345", "Email":"eee@322.3352", 
-					"Name":"55", "School":"YZU", "Department":"Computer"}', "1ddd11");
+$carPart -> SaveRecommendationLetter('1ssd33344sds 555', "1ddd11");
 
 //Call the function with the "->" operator
 //This time with the argument "Black Color"
