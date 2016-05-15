26051a0687a24a1387aada0be9488081d34ee917
HarrisQs <harris32916@gmail.com>
2016-05-15 18:00:01 +0800

    basic upload UI is OK.

--- Upload Sub-system/RecommendationLetter.php
+++ Upload Sub-system/RecommendationLetter.php
@@ -1,5 +1,5 @@
 <?php
-	//2016/05/12
+//2016/05/12
 //Programmer：張弘瑜
 //推薦信存到資料庫的部分
 	include_once"../Respository Sub-system/CennectDB.php"
@@ -10,19 +10,24 @@
 		private $FileAddress;
 		function __construct()
 		{
-
+			$this->DBLink = new ConnectDB();
 		}
 		function __destruct()
 		{
 
 		}
-		public function SaveRecommendationLetter($fileName)
+		public function SaveRecommendationLetter($fileName)//把參數設定好 並且呼叫儲存的
 		{
-
+			$this->FileName = $fileName;
+			$this->FileAddress = "";
+			while(!$this->SaveToDB()){}//直到成功存入DB為止
 		}
-		public function SaveToDB()
+		public function SaveToDB()//負責來存進資料庫的
 		{
-			
+			if($this->DBLink->SaveRecommendationLetter($this->FileName, $account))
+				return true;
+			else
+				return false;
 		}
 
 	}
---
+++ Upload Sub-system/UploadRecommendationLetter.php
@@ -0,0 +1,23 @@
+<?php
+//2016/05/15
+//Programmer：張弘瑜
+//推薦信上傳的部分
+	include_once"../Respository Sub-system/CennectDB.php"
+	class UploadRecommendationLetter
+	{
+		private $RecommendationLink;
+		function __construct()
+		{
+			$this->RecommendationLink = new RecommendationLetter();
+		}
+		function __destruct()
+		{
+
+		}
+		public function Upload()//判斷是否上傳成功
+		{
+			
+		}
+	}
+
+?>
