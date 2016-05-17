153fd72a0a633e89e44415cc06ea17f7784003bc
Guo Po Hong <steven558877@gmail.com>
2016-05-18 03:32:02 +0800

    Change the method to get teacher's account from assign by myself to get from LogIn.php

--- ReviewApplication Sub-system/ReviewIndex.php
+++ ReviewApplication Sub-system/ReviewIndex.php
@@ -1,10 +1,10 @@
 <html>
 <?php 
-  /*include_once"../AccountManager-Sub-system/LogIn.php";
+  include_once"../AccountManager-Sub-system/LogIn.php";
   $login = new LogIn;
 	$TeacherID = $login->getaccount();
-  */
-  $TeacherID = "adminsteven";
+  
+  //$TeacherID = "adminsteven";
 ?>
 <body>
 <form name="action" action="Review.php" method="post">
