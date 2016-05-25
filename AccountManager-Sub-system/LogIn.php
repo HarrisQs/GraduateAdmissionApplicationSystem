47f7d879bf9a984721853b5eeaed25cdf1bdfc72
Miranda84315 <miranda84315@gmail.com>
2016-05-25 22:03:35 +0800

    modify log in

--- AccountManager-Sub-system/LogIn.php
+++ AccountManager-Sub-system/LogIn.php
@@ -1,5 +1,6 @@
 <?php
 	include_once "../Repository Sub-system/AccountDB.php";
+	include_once "../Repository Sub-system/ConnectDB.php";
     $ID = $_POST["ID"];
     $password = $_POST["password"];
 	
@@ -8,37 +9,24 @@
 	
 	class LogIn
 	{
+		private $DataBase;
+		private $Accountdata;
 		function __construct() 
 		{
      		 //$this->AccountDB = new AccountDB();
    		}
 		Public Function LoginSystem($ID,$password)
 		{
-			/*
-			if($this->AccountDB->ValidateAccount($iD, $password))
-				echo "welcome ~";
-			else
-				echo "LogIn fail!";*/
-			
-			if ( !( $database = mysql_connect( "localhost", "se", "se" ) ) )
-			die( "Could not connect to database " );
-			if ( !mysql_select_db( "se2", $database ) )
-			die( "Could not open products database " );
-		
 			$sql="SELECT `pass` FROM `account_data` WHERE `account`='".$ID."'";	
-			$result=mysql_query($sql);	
-			$row_result=mysql_fetch_assoc($result);
-			if(isset($ID))
-			{
-						//將讀取出來的資料取出欄位名稱為username的資料，並且存在$admin
-				$admin=$row_result["pass"];
-				if($password==$admin)
-					echo "welcome ~";
-				else
-					echo "LogIn fail!";
-				
-			}
-			mysql_close( $database );
+			
+			$this->Accountdata = new AccountDB();
+			$this->DataBase = new ConnectDB();
+			
+			$result = $this->Accountdata->ValidateAccount($ID,$password);
+			if($result)
+				echo "YES";
+			else
+				echo "no";
 			
 		}
 	}
