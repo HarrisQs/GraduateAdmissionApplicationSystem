2138e9bba672af9c275289238c9228273fe73a66
Rita <rita20839@gmail.com>
2016-06-01 12:33:58 +0800

    I hate json_decode

--- ApplicationManager Sub-system/StaticPage.php
+++ ApplicationManager Sub-system/StaticPage.php
@@ -4,15 +4,22 @@
 	include_once "../Repository Sub-system/ApplicationDB.php";
 	$Database = new ApplicationDB();
 	$DE;
-	//$DE = json_decode('{"foo":"bar"}',true);
+	//var_dump(json_decode('{"foo":"bar","aaa":"bbb"}'));
 	//echo $DE['foo'];
-	//$DE = (json_decode($Database->GetLastHistory("Rita1234"),true));
 	$DE = $Database->GetLastHistory("Rita1234");
-	$decode=json_decode($DE,true);
-	echo $decode['account'];
+	//echo $DE;
+	//echo $DE;
+	$DE = str_replace(']', '', $DE);
+	$DE = str_replace('[', '', $DE);
+	$DE = str_replace('\"', '"', $DE);
+	$DE[0]='\'';
+	$DE[256]='\'';
+	echo $DE;
+	var_dump(json_decode($DE));
+	//echo $DE[0];
 	//echo $Database->GetLastHistory("Rita1234");
 	//echo $DE[0];
-	if($Database->GetLastHistory("currentAccount")=="You don’t have last history")
+	if($Database->GetLastHistory($currentAccount)=="You don’t have last history")
 		echo "Test OK";
 	else
 ?>
