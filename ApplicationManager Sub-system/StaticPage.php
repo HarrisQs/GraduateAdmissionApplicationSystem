37813f7286a8a01fc6bf6e9fcebb89d686e8c89f
rita20839 <rita20839@gmail.com>
2016-06-06 18:28:47 +0800

    OK

--- ApplicationManager Sub-system/StaticPage.php
+++ ApplicationManager Sub-system/StaticPage.php
@@ -54,186 +54,86 @@
 ?>
 <html>
     <head>
-        <title>Software Engineering</title>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <link rel='stylesheet' type='text/css'>
-            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-            <script src="js/skel.min.js"></script>
-            <script src="js/skel-panels.min.js"></script>
-            <script src="js/init.js"></script>
-            <noscript>
-                <link rel="stylesheet" href="css/skel-noscript.css" />
-                <link rel="stylesheet" href="css/style.css" />
-                <link rel="stylesheet" href="css/style-desktop.css" />
-            </noscript>
-            </head>
-    <body class="no-sidebar">
-        
-        <!-- Header -->
+  <meta charset="utf-8">
+  <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> -->
+  <title>Software Engineering</title>
+  <!-- <meta http-equiv="content-type" content="text/html; charset=utf-8" /> -->
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/skel.min.js"></script>
+  <script src="js/skel-panels.min.js"></script>
+  <script src="js/init.js"></script>
+  <link rel="stylesheet" href="css_2/style.css">
+  <link rel="stylesheet" href="css/skel-noscript.css" />
+  <link rel="stylesheet" href="css/style.css" /> 
+  <link rel="stylesheet" href="css/style-desktop.css" />
+  <link rel="stylesheet" type="text/css" href="css_3/demo.css" />
+  <link rel="stylesheet" type="text/css" href="css_3/style.css" />
+  <!-- [if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif] -->
+</head>
+<body>
         <div id="header">
-            <div class="container">
-                
-                <!-- Logo -->
-                <div id="logo" style="font-size:11px;">
-                    <h1><a href="#">Web-based graduate <br>admission application system</a></h1>
-                </div>
-                
-                <!-- Nav -->
-                <nav id="nav">
-                    <ul>
-                        <li><a href="../AccountManager-Sub-system/index.html">sign out</a></li>
-                        <li class="active"><a href="student.html">Student</a></li>
-                    </ul>
-
-                </nav>
-                
-            </div>
+        <div class="container">
+        <div id="logo" style="font-size:11px;">
+          <h1><a href="#">Web-based graduate <br>admission application system</a></h1>
         </div>
-        <!-- Header -->
-        
-        <!-- Banner -->
+        <nav id="nav">
+           <ul>
+              <li><a href="../AccountManager-Sub-system/index.html">sign out</a></li>
+              <li class="active"><a href="student.html">Student</a></li>
+           </ul>
+        </nav>
+                
+        </div>
+        </div>
+
+
         <div id="banner">
             <div class="container">
             </div>
         </div>
-        <!-- /Banner -->
-        
-        <!-- Main -->
-        <div id="page">
-            
-            <!-- Main -->
-            <div id="main" class="container">
-                <div class="row">
-                    <div class="12u">
-                        <section>
-                            <header>
-                                <h2 align='center'>Application Form</h2>&nbsp;
-                                <!-- <span class="byline"> -->
-                                    <FORM name="basicData" method="POST" action="FillOutData.php" enctype="multipart/form-data">
+        <!-- <div id="container_demo"> -->
+        <!-- <div id="page" > -->
 
-                                    <CENTER>
-                                    <TABLE width='600' align='center' border='10' style="border:3px #FFD382 dashed;" cellpadding="10">
-                
-                                        <!-- <TR bgcolor='#7700FF' valign='center'> -->
-                                            <!-- <H3 bgcolor='#B088FF' >基本資料填寫</H3> -->
-                                        <!-- </TR> -->
+    <br><br><br>
+    <header><h2 align="center">Application Form</h2></header>
 
-                                        <TR bgcolor='#0044BB' valign='center'>
-                                            <TD align='center' height='32' width='100'></TD>
-                                            <TD align='center'></TD>  
-                                        </TR>
+    <!-- <div id="login" class="animate form"> -->
+    <!-- <div id="animate form"> -->
+    <!-- <div id="wrapper"> -->
+    <!-- <div id="page"></div> -->
+    <div id="wrapper">
+    <FORM name="basicData" method="POST" action="FillOutData.php" enctype="multipart/form-data" >
 
-                                        <TR>
-                                            <TD bgcolor='#5555FF' valign='center' align='center' height='32'>
-                                            <font color="#DDDDDD">學生姓名</font></TD>
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userName?>
-                                        </TR>
+    <h1 >基本資料填寫</h1>
 
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                            <TD align='center' height='32'>
-                                            <font color="#DDDDDD">電子信箱</font>
-                                            </TD >
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userEmail?>
-                                        </TR>
+    <font class="about">學生姓名　</font>　　　<?php echo $userName?><br><br>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                                <TD align='center' height='32'>
-                                                <font color="#DDDDDD">學校</font>
-                                                </TD>
-                                                <TD bgcolor='#009FCC' align='center'><?php echo $userSchool?>
-                                               
-                                        </TR>
+    <font class="about">電子信箱　</font>　　　<?php echo $userEmail?><br><br>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                                <TD align='center' height='32'>
-                                                <font color="#DDDDDD">系所</font>
-                                                </TD>
-                                                <TD bgcolor='#009FCC' align='center'><?php echo $userDepartment?>
-                                        </TR>
+    <font class="about">就讀學校　</font>　　　<?php echo $userSchool?><br><br>
+
+    <font class="about">　系　所　</font>　　　<?php echo $userDepartment?><br><br>
 
 
-                                        <TR bgcolor='#0044BB' valign='center'>
-                                            <TD>
-                                            <TD>
-                                            <font color="#DDDDDD">＋＋＋＋＋＋＋＋＋＋＋申請資料填寫＋＋＋＋＋＋＋＋＋＋＋</font>
-                                            </TD>
-                                            </TD>
-                                        </TR>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                            <TD align='center' height='32' width='100'></TD>
-                                            <TD align='center'><font color="#DDDDDD"></font>
-                                            </TD>  
-                                        </TR>
+    <h1>申請資料填寫</h1>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                            <TD align='center' height='32'>
-                                            <font color="#DDDDDD">教授電子信箱</font>
-                                            </TD>
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userTeacher?>
-                                        </TR>
+    <font class="about">教授信箱　</font>　　　<?php echo $userTeacher?><br><br>
 
+    <font class="about">　動　機　</font>　　　<?php echo $userSOP?><br><br>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                            <TD align='center' height='32'>
-                                            <font color="#DDDDDD">動機簡述</font>
-                                            </TD>
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userSOP?>
-                                        </TR>
+    <font class="about">申請系所　</font>　　　<?php echo $userProgramSelection?><br><br>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                            <TD align='center' height='32'>
-                                            <font color="#DDDDDD">申請系所</font>
-                                            </TD>
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userProgramSelection?>
-                                        </TR>
+    <font class="about">　自　傳　</font>　　　
+    <?php echo $userCV?><br><br>
 
-                                        <TR bgcolor='#5555FF' >
-                                            <TD align='center' height='10'>
-                                            <font color="#DDDDDD">自傳</font>
-                                            </TD>
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userCV?>
-                                        </TR>
+    <font class="about">上傳成績單</font>　　　<?php echo $userTranscript?><br><br>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                            <TD align='center' height='32'>
-                                            <font color="#DDDDDD">上傳成績單</font>
-                                            </TD>
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userTranscript?>
-                                        </TR>
+  </form>
+  </div>
 
-                                        <TR bgcolor='#5555FF' valign='center'>
-                                            <TD align='center' height='32'>
-                                            <font color="#DDDDDD">審查狀態</font>
-                                            </TD>
-                                            <TD bgcolor='#009FCC' align='center'><?php echo $userStatus?>
-                                        </TR>
-
-                                    </TABLE>
-                                    </CENTER>
-                                    
-                                </FORM>
-                                <!-- </span> -->
-                            </header>
-                        </section>
-                    </div>
-                    
-                </div>
-            </div>
-            <!-- Main -->
-            
-        </div>
-        <!-- /Main -->
-        
-        
-        <!-- Copyright -->
-        <div id="copyright" class="container">
-            Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
-        </div>
-        
-        
-    </body>
+</body>
 </html>
